3be0fe22de045b3964b6c42b8742e1679f5b3e55
Puneeth <puneeth@leapsurge.in>
2024-01-24 21:45:45 +0530

    Added Live Stream Video Through LiveKIT.

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,9 +1,34 @@
+import StreamPlayer from "@/components/stream-player";
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs";
+
+interface CreatorPageProps {
+  params: {
+    username: string;
+  }
+}
 
 
+const CreatorPage = async({
+  params,
+}: CreatorPageProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username)
 
-const CreatorPage = () => {
+
+  if(!user || user.externalUserId !== externalUser?.id || !user.stream) {
+  throw new Error("Unauthorized");
+  }
+
+
   return (
-    <div>Creator Page</div>
+    <div className="h-full">
+      <StreamPlayer 
+        user={user}
+        stream={user.stream}
+        isFollowing={false}
+      />
+    </div>
   )
 }
 
